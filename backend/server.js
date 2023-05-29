730384d88a73f3556d06695f6ff4b3dabe030f3b
Arsalan Ali <aliarsalan.dev@gmail.com>
2023-05-29 19:54:38 +0500

    resolved build directory issue

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ app.use("/api/nft", nftRouter)
 // app.use("/api/mint", mintRouter)
 
 const __dirname = path.resolve()
-app.use(express.static(path.join(__dirname, "/client/build")))
+app.use(express.static(path.join(__dirname, "/frontend/build")))
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 )
 
 app.get("/", (req, res) => {
